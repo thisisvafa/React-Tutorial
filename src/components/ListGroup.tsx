b44bd30008deebf97f7a77c393a192740a852350
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-03-16 14:18:39 +0330

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,36 @@
 // import { Fragment } from "react";
 
 function ListGroup() {
-    const items = [
+    // const items = [
+    //     'New York',
+    //     'San Francisco',
+    //     'Tokyo',
+    //     'London',
+    //     'Paris'
+    // ];
+
+    let items = [
         'New York',
         'San Francisco',
         'Tokyo',
         'London',
         'Paris'
     ];
+    // items = [];
+
+    // if (items.length === 0)
+    //     return (
+    //         <>
+    //             <h1>List</h1>
+    //             <p>No item found</p>
+    //         </>
+    //     )
+
+    // const message = items.length === 0 ?  <p>No item found</p> : null;
+
+    const getMessage = () => {
+        return items.length === 0 ?  <p>No item found</p> : null;
+    }
 
     return (
         <>
@@ -19,9 +42,18 @@ function ListGroup() {
                 <li className="list-group-item">A fourth item</li>
                 <li className="list-group-item">And a fifth one</li>
             </ul> */}
+            {/* { items.length === 0 ?  <p>No item found</p> : null } */}
+            {/* {message} */}
+            {getMessage()}
             <ul className="list-group">
                 {items.map((item) => (
-                    <li key={item} className="list-group-item">{item}</li>
+                    <li 
+                        key={item}
+                        className="list-group-item"
+                        onClick={() => console.log('Clicked')}
+                    >
+                        {item}
+                    </li>
                 ))}
             </ul>
         </>
