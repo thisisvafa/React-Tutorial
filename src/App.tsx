c182d0a7df254a3f6677fc35554598d59c89768d
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-03-18 09:10:56 +0330

    Passing Data via Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 // import Message from "./Message";
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+import ListGroups from "./components/ListGroups";
 
 function App() {
+  let items = [
+    'New York',
+    'San Francisco',
+    'Tokyo',
+    'London',
+    'Paris'
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
   // return <div><Message /></div>;
-  return <div><ListGroup /></div>;
+  // return <div><ListGroup /></div>;
+  return <div><ListGroups items={items} heading="Cities" onSelectItem={handleSelectItem} /></div>;
 }
 
 export default App;
---
+++ src/components/ListGroups.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+interface Props {
+    items: string[];
+    heading: string;
+    onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+    const getMessage = () => {
+        return items.length === 0 ?  <p>No item found</p> : null;
+    }
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    return (
+        <>
+            <h1>{heading}</h1>
+            {getMessage()}
+            <ul className="list-group">
+                {items.map((item, index) => (
+                    <li 
+                        key={item}
+                        className={
+                            selectedIndex === index
+                                ? "list-group-item active"
+                                : "list-group-item"  
+                        }
+                        onClick={ () => {
+                            setSelectedIndex(index);
+                            onSelectItem(item);
+                        }}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
