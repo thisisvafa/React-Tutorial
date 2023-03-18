a73733738c32bffab7acec7fd16c1cf8c2f7c2c8
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-03-18 10:34:26 +0330

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 // import Message from "./Message";
 // import ListGroup from "./components/ListGroup";
-import ListGroups from "./components/ListGroups";
+// import ListGroups from "./components/ListGroups";
+
+import Alert from "./components/Alert";
+
 
 function App() {
-  let items = [
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris'
-  ];
+  // let items = [
+  //   'New York',
+  //   'San Francisco',
+  //   'Tokyo',
+  //   'London',
+  //   'Paris'
+  // ];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // }
 
   // return <div><Message /></div>;
   // return <div><ListGroup /></div>;
-  return <div><ListGroups items={items} heading="Cities" onSelectItem={handleSelectItem} /></div>;
+  // return <div><ListGroups items={items} heading="Cities" onSelectItem={handleSelectItem} /></div>;
+  // return <div><Alert text="Hello World" /></div>;
+  return (
+    <div>
+      <Alert>Hello World</Alert>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Alert.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+interface Props {
+    // children: string;
+    children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return (
+    <div className='alert alert-danger'>{children}</div>
+  )
+}
+
+export default Alert
